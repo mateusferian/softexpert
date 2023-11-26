92d02fa115f7874ef270dab29df030652a2cfb5a
mateusferian <mateusferian10@gmail.com>
2023-11-26 11:15:02 -0300

    :tada: initial commit

---
+++ src/main/java/br/com/mateusferian/softexpert/SoftexpertApplication.java
@@ -0,0 +1,13 @@
+package br.com.mateusferian.softexpert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftexpertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftexpertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mateusferian/softexpert/SoftexpertApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mateusferian.softexpert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftexpertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
